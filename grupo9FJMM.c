fa0e62a575e8d87055863890b91a4178e3042021
Felipe Gabriel <fesenapeschi2013@gmail.com>
2026-06-23 22:30:32 -0300

    Criei as estruturas básicas que serão usadas na criação das tabelas hash e na criação do grafo!

--- grupo9FJMM.c
+++ grupo9FJMM.c
@@ -10,20 +10,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct Autor {
+    int id;
+    char nome[100];
+} Autor;
+
+typedef struct Paper {
+    int id;
+    char titulo[150];
+    int *autores;
+} Paper;
+
+// Vértice do grafo
+typedef struct AutorNode {
+    int AutorId;
+    struct AutorNode* prox;  
+} AutorNode;
+
+typedef struct Aresta {
+    struct AutorNode* destino;
+    PaperNode* papers;
+    struct Aresta* prox;    
+} Aresta;
+
+// Dados da Aresta
+typedef struct PaperNode {
+    int PaperId;
+    struct PaperNode* prox;
+} PaperNode;
+
 int main(){
 
     // Estrutura do Arquivo:
     // Nome do pesquisador "tab" Titulo
 
     // TODO: Passos Iniciais: 
-    // - Ler Arquivo
-    // - Implementar grafo
+    // - ✅ Ler Arquivo
+    // - ✅ Definir estruturas(definições básicas de tabela hash e de nós para o grafo)
 
+    // TODO: Passos estruturais
+    // - Criar tabela hash para autores
+    // - Criar tabela hash para papers
+    // - Criar grafo.
 
-    // Abre arquivo
+    // TODO: Passos de analise
+
     FILE *arquivo_pesq = fopen("dadosPesquisadores.txt", "r");
 
-    // Valida a abertura do arquivo
     if(arquivo_pesq == NULL){
         printf("Erro ao ler arquivo");
         return 1;
